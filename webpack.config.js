e3bf36bb078a4dd42e7b445f63680d65176d9780
Poonacha <ppoonacha@c02v41jgg8wl.limited.brands.com>
2018-03-09 00:58:30 +0530

    added path, html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 //since webpack has lot of configurations we keep all the webpack related settings in this file and execute from package.json file scripts
+//we use the html webpack plugin to create the html page template dynamically
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: path.resolve(__dirname, './dist'),
+        filename: 'app.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
 }
