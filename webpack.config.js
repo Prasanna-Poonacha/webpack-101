e76a714b698ef5c72e24c75da62600793c0af80a
Poonacha <ppoonacha@c02v41jgg8wl.limited.brands.com>
2018-03-09 01:21:03 +0530

    fixed indentation

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,14 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'app.bundle.js'
     },
-    plugins: [new HtmlWebpackPlugin({
-        title: "Webpack demo",
-        minify: {
-            collapseWhitespace: true
-        },
-        hash: true,
-        template:"./src/index.html"
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack demo",
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template:"./src/index.html"
+        })
+    ]
 }
