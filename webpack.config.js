a8a2c5acf70daf30d9bf51278de3947f9815d030
Poonacha <ppoonacha@c02v41jgg8wl.limited.brands.com>
2018-03-09 12:19:46 +0530

    added style and used css loader and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,12 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            // {test: /\.css$/, use: 'css-loader'}
+            {test: /\.css$/, loaders: 'style-loader!css-loader'}
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack demo",
